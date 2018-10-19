e945af53995441339b7d93b7bd8554bb51425a19
AndreaMariaM <31991092+AndreaMariaM@users.noreply.github.com>
2018-10-19 14:47:02 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*main.cpp, Project 3a.
+Andrea Matellian and Nathan Newbury
+ID: MATNEW
+this file contains the main program
+*/
+
+#include <iostream>
+#include "dictionary.h"
+
+using namespace std;
+
+int main
+{
+	dictionary play;
+
+	return 0;
+}
